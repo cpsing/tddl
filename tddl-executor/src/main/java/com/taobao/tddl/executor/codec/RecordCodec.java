65aefb8e6d31f3a2e15e5025ce33af313edfa893
mengshi.sunmengshi <mengshi.sunmengshi@taobao.com>
2014-01-20 17:18:18 +0800

    删掉maprecord

---
+++ tddl-executor/src/main/java/com/taobao/tddl/executor/codec/RecordCodec.java
@@ -0,0 +1,17 @@
+package com.taobao.tddl.executor.codec;
+
+import com.taobao.tddl.executor.record.CloneableRecord;
+
+/**
+ * @author mengshi.sunmengshi 2013-12-2 下午6:25:22
+ * @since 5.1.0
+ */
+public interface RecordCodec<T> {
+
+    byte[] encode(CloneableRecord record);
+
+    CloneableRecord decode(byte[] bytes);
+
+    CloneableRecord newEmptyRecord();
+
+}
