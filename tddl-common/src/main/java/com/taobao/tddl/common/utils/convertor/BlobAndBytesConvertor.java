30338c69916768d0444696094a41a5a4d6789f00
agapple <jianghang115@gmail.com>
2014-01-21 18:35:51 +0800

    提交下转化类

---
+++ tddl-common/src/main/java/com/taobao/tddl/common/utils/convertor/BlobAndBytesConvertor.java
@@ -0,0 +1,38 @@
+package com.taobao.tddl.common.utils.convertor;
+
+import java.sql.Blob;
+import java.sql.SQLException;
+
+/**
+ * Blob <-> bytes类型之间的转化
+ * 
+ * @author jianghang 2014-1-21 下午6:15:01
+ * @since 5.1.0
+ */
+public class BlobAndBytesConvertor {
+
+    /**
+     * Blob -> bytes 转化
+     */
+    public static class BlobToBytes extends AbastactConvertor {
+
+        @Override
+        public Object convert(Object src, Class destClass) {
+            if (Blob.class.isInstance(src) && destClass.equals(byte[].class)) {
+                if (src == null) {
+                    return null;
+                } else {
+                    try {
+                        Blob blob = (Blob) src;
+                        return blob.getBytes(0, (int) blob.length());
+                    } catch (SQLException e) {
+                        throw new ConvertorException(e);
+                    }
+                }
+            }
+
+            return src != null ? src.toString() : null;
+        }
+    }
+
+}
--- tddl-optimizer/src/main/java/com/taobao/tddl/optimizer/core/datatype/BytesType.java
+++ tddl-optimizer/src/main/java/com/taobao/tddl/optimizer/core/datatype/BytesType.java
@@ -1,8 +1,12 @@
 package com.taobao.tddl.optimizer.core.datatype;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.google.common.primitives.Bytes;
 import com.taobao.tddl.common.exception.NotSupportException;
 import com.taobao.tddl.common.exception.TddlRuntimeException;
+import com.taobao.tddl.common.model.BaseRowSet;
 
 /**
  * {@link Bytes} 类型
@@ -10,7 +14,7 @@ import com.taobao.tddl.common.exception.TddlRuntimeException;
  * @author mengshi.sunmengshi 2014年1月21日 下午5:16:00
  * @since 5.1.0
  */
-public class BytesType extends CommonType<byte[]> {
+public class BytesType extends AbstractDataType<byte[]> {
 
     @Override
     public int encodeToBytes(Object value, byte[] dst, int offset) {
@@ -55,6 +59,28 @@ public class BytesType extends CommonType<byte[]> {
     @Override
     public byte[] getMinValue() {
         return new byte[] { Byte.MIN_VALUE };
+    }
+
+    @Override
+    public ResultGetter getResultGetter() {
+        return new ResultGetter() {
+
+            @Override
+            public Object get(ResultSet rs, int index) throws SQLException {
+                return rs.getBytes(index);
+            }
+
+            @Override
+            public Object get(BaseRowSet rs, int index) {
+                Object val = rs.getObject(index);
+                return convertFrom(val);
+            }
+        };
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
     }
 
     @Override
