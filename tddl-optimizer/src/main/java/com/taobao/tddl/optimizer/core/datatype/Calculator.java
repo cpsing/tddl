8227165dc7a4f3f3ecbe526a17bdb82003de6454
mengshi.sunmengshi <mengshi.sunmengshi@taobao.com>
2014-01-21 15:05:42 +0800

    每个type增加calculator

---
+++ tddl-optimizer/src/main/java/com/taobao/tddl/optimizer/core/datatype/Calculator.java
@@ -0,0 +1,7 @@
+package com.taobao.tddl.optimizer.core.datatype;
+
+public interface Calculator {
+
+    Object add(Object v1, Object v2);
+
+}
---
+++ tddl-optimizer/src/main/java/com/taobao/tddl/optimizer/core/datatype/DataTypeUtil.java
@@ -0,0 +1,41 @@
+package com.taobao.tddl.optimizer.core.datatype;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import com.taobao.tddl.common.exception.TddlRuntimeException;
+
+public class DataTypeUtil {
+
+    public static DataType getTypeOfObject(Object v) {
+
+        if (v == null) return DataType.NullType;
+
+        if (v instanceof Integer) return DataType.IntType;
+        if (v instanceof Long) return DataType.LongType;
+        if (v instanceof Short) return DataType.ShortType;
+        if (v instanceof Float) return DataType.FloatType;
+        if (v instanceof Double) return DataType.DoubleType;
+        if (v instanceof BigDecimal) return DataType.BigDecimalType;
+
+        if (v instanceof String) return DataType.StringType;
+
+        if (v instanceof Date) return DataType.DateType;
+        if (v instanceof Time) return DataType.TimeType;
+        if (v instanceof Timestamp) return DataType.TimestampType;
+
+        if (v instanceof Byte) return DataType.ByteType;
+        if (v instanceof Byte[]) return DataType.BytesType;
+        if (v instanceof Boolean) return DataType.BooleanType;
+
+        if (v instanceof Blob) return DataType.BlobType;
+        if (v instanceof Clob) return DataType.ClobType;
+
+        throw new TddlRuntimeException("type: " + v.getClass().getSimpleName() + " is not supported");
+
+    }
+}
